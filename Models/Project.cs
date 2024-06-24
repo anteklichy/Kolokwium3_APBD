6467f75b3816dce77485a48446710ca9665d1f0d
anteklichy <128155700+anteklichy@users.noreply.github.com>
2024-06-24 09:41:15 +0200

    Delete Models/Project.cs Models/Access.cs
    useless

--- Models/Project.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Kolokwium.Models
-{
-    public class Project
-    {
-        public int IdProject { get; set; }
-        public string? Name { get; set; }
-        public int IdDefaultAssignee { get; set; }
-    }
-}
